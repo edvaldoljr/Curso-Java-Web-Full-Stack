ff4bade0c23b7ff25bddffa63e163a8914968c63
edvaldojr <edvaldoluizj@gmail.com>
2022-04-22 13:19:20 -0300

    Switch case

---
+++ curso-java-web/src/modulo8/Aula04.java
@@ -0,0 +1,24 @@
+package modulo8;
+
+public class Aula04 {
+
+	  //Imprima os dias da semana 1 como domingo
+    public static void main(String[] args) {
+
+        System.out.println("Digite um dia da semana em números");
+        int dia = 8;
+        
+        switch (dia) {
+        case 1 -> System.out.println("Domingo");
+        case 2 -> System.out.println("Segunda");
+        case 3 -> System.out.println("Terça");
+        case 4 -> System.out.println("Quarta");
+        case 5 -> System.out.println("Quinta");
+        case 6 -> System.out.println("Sexta");
+        case 7 -> System.out.println("Sábado");
+	
+		default ->
+		throw new IllegalArgumentException("Numero invalido " + dia);
+		}
+    }
+}
